987fcaf97153d144731fcdbcf610afd82196ff92
Ange Tato <angetato@gmail.com>
2019-04-06 03:22:45 -0400

    Update mlp.py

--- mlp.py
+++ mlp.py
@@ -4,12 +4,13 @@ from keras import models
 from keras import layers
 from adam import Adam
 from aadam import AAdam
-from adamW import AdamW
-from sgd_cust import SGDCust
+from sgd import SGD
+from asgd import ASGD
+from adagrad import Adagrad
+from aadagrad import AAdagrad
 import numpy as np 
 import pandas as pd
 
-adamw = AdamW(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0., weight_decay=0.025, batch_size=1, samples_per_epoch=1, epochs=1)
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
@@ -29,9 +30,11 @@ results_acc = []
 result_acc= []
 results_loss = []
 result_loss = []
-test_results = []
-test_result = []
-l= [Adam(amsgrad = False), Adam(amsgrad = True), AAdam(amsgrad = False), AAdam(amsgrad = True)]
+test_acc_results = []
+test_loss_results = []
+
+l= [Adagrad(),AAdagrad(),SGD(),ASGD(), Adam(amsgrad = True), AAdam(amsgrad = True),Adam(amsgrad = False), AAdam(amsgrad = False) ]
+
 for opt in l:
 
     network.compile(optimizer= opt,
@@ -42,28 +45,45 @@ for opt in l:
     initial_weights = network.get_weights()
     result_acc = []
     result_loss = []
-    test_result = []
+    test_loss = []
+    test_acc = []
     for i in range (20):
-        network.set_weights(initial_weights)
-        history = network.fit(train_images, train_labels, epochs=20, batch_size=128,verbose=0)
-        test_loss, test_acc = network.evaluate(test_images, test_labels)
-        test_result.append([test_loss, test_acc])
-        result_acc.append(history.history['acc'])
-        result_loss.append(history.history['loss'])
+        network.load_weights('initial_weights.h5')
+        result_acc_e = []
+        result_loss_e = []
+        test_acc_e = []
+        test_loss_e = []
+        for j in range (10):
+            history = network.fit(train_images, train_labels, epochs=1, batch_size=128,verbose=0)
+            '''if j % 2 == 0 :
+                test_loss_j, test_acc_j = network.evaluate(test_images, test_labels)
+                test_acc_e.append(test_acc_j)
+                test_loss_e.append(test_loss_j)'''
+            result_acc_e.append(history.history['acc'][0])
+            result_loss_e.append(history.history['loss'][0])
+        #print(result_loss_e)
+        test_loss.append(test_loss_e)
+        test_acc.append(test_acc_e)
+        result_acc.append(result_acc_e)
+        result_loss.append(result_loss_e)
     print("##### NEW OPTIMIZER #####")
     print(np.mean(result_acc,axis=0))
     print(np.mean(result_loss,axis=0))
-    print(np.mean(test_result,axis=0))
+    print(np.mean(test_acc,axis=0))
+    print(np.mean(test_loss,axis=0))
     results_acc.append(np.mean(result_acc,axis=0))
     results_loss.append(np.mean(result_loss,axis=0))
-    test_results.append(np.mean(test_result,axis=0))
+    test_acc_results.append(np.mean(test_acc,axis=0))
+    test_loss_results.append(np.mean(test_loss,axis=0))
 
-df = pd.DataFrame(results_acc)
-df.to_csv("acc_train_mlp.csv")
+
+""" df = pd.DataFrame(results_acc)
+df.to_csv("resultsECML/acc_train_mlp.csv")
 df = pd.DataFrame(results_loss)
-df.to_csv("loss_train_mlp.csv")
-df = pd.DataFrame(test_results)
-df.to_csv("results_test_mlp.csv") 
-
+df.to_csv("resultsECML/loss_train_mlp.csv")
+df = pd.DataFrame(test_acc_results)
+df.to_csv("resultsECML/acc_test_mlp.csv")
+df = pd.DataFrame(test_loss_results)
+df.to_csv("resultsECML/loss_test_mlp.csv")  """
 
 
